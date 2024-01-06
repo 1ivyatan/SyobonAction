f4c4bec5054efd185882f423386fc7b94938dbca
1ivyatan <143333830+1ivyatan@users.noreply.github.com>
2024-01-06 20:24:41 +0200

    arguments

--- src/loader.c
+++ src/loader.c
@@ -1,7 +1,17 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int SOUND = 1;
 
 void checkarguments(int argc, char **argv) {
-    
+    if (argc < 2) return;
+    if (argc > 2) {
+        printf("Too many arguments\n");
+        return;
+    }
+
+    if (!strcmp(argv[1], "-nosound")) SOUND = 0;
 }
 
 int loadeverything(int sound) {
