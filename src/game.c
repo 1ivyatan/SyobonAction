2a90b84e8c834a99843d2d6630f23c683684b342
1ivyatan <143333830+1ivyatan@users.noreply.github.com>
2024-01-10 08:07:41 +0200

    stealing from raylibs example to understand how to camera works

--- src/game.c
+++ src/game.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <raylib.h>
+#include <raymath.h>
 #include "activities.h"
 #include "game.h"
 #include "graphics.h"
@@ -20,15 +21,40 @@ activity *currentactivity = NULL;
 /* video */
 Camera2D playercamera;
 
-/* tempppppppp */
-entity playerentity;
-
 /* functions */
 /* textures */
 void applytexturetoentity(entity *thing, spritetexture texture) {
     thing->texture = texture;
 }
 
+/* things from raylib example */
+
+#define PLAYER_JUMP_SPD 350.0f
+#define PLAYER_HOR_SPD 200.0f
+
+typedef struct Player {
+    Vector2 position;
+    float speed;
+    bool canJump;
+} Player;
+
+ Camera2D camera = { 0 };
+         Player player = { 0 };
+
+void UpdatePlayer(Player *player, float delta)
+{
+    if (IsKeyDown(KEY_LEFT)) player->position.x -= PLAYER_HOR_SPD*delta;
+    if (IsKeyDown(KEY_RIGHT)) player->position.x += PLAYER_HOR_SPD*delta;
+    if (IsKeyDown(KEY_SPACE) && player->canJump)
+    {
+        player->speed = -PLAYER_JUMP_SPD;
+        player->canJump = false;
+    }
+
+}
+
+
+////////
 /* loop */
 void loopgame(void) {
     while (!WindowShouldClose()) {
@@ -45,23 +71,47 @@ void loopgame(void) {
 /* ACTIVITY FUNCTIONS */
 
 void gamelogic() {
+    float deltaTime = GetFrameTime();
+
+     UpdatePlayer(&player, deltaTime);
+     
+
+    camera.offset = (Vector2){ 480/2.0f, 420/2.0f };
+    camera.target = player.position;
+
 
 }
 
 void gamedraw() {
     ClearBackground(SYOBONSKYCOLOR);
-    BeginMode2D(playercamera);
+    
 
-    EndMode2D();
+            BeginMode2D(camera);
+                DrawTextEx(sazanamifont, "bg!!!!!!!", (Vector2){300, 300}, 64, 1, BLACK);
+                Rectangle playerRect = { player.position.x - 20, player.position.y - 40, 40, 40 };
+                DrawRectangleRec(playerRect, RED);
+
+            EndMode2D();
+
+
+
 }
 
 void startlogic() {
     if (IsKeyPressed(KEY_ENTER)) {
         currentactivity = &gameactivity;
 
-        applytexturetoentity(&playerentity, creaturetex);
-        playerentity.x = 140;
-        playerentity.y = 300;
+        ////////////////////////
+    player.position = (Vector2){ 400, 280 };
+    player.speed = 0;
+    player.canJump = false;
+
+
+camera.target = player.position;
+    camera.offset = (Vector2){ 480/2.0f, 420/2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
     }
 }
 
